b056902556e3b8eedf14d4cc9ecf07c4ef2c52d5
Caayu <henrycay@outlook.com>
2022-05-15 23:54:44 -0300

    Descrições de queries e tipos

--- lib/food_diary_web/schema/types/user.ex
+++ lib/food_diary_web/schema/types/user.ex
@@ -1,8 +1,9 @@
 defmodule FoodDiaryWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
+  @desc "Logic user representation"
   object :user do
-    field :id, non_null(:id)
+    field :id, non_null(:id), description: "Users id, needs to be an integer."
     field :name, non_null(:string)
     field :email, non_null(:string)
   end
