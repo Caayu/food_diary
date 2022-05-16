3dc10fd0845c22d3262dfa1aa319233a2aa44075
Caayu <henrycay@outlook.com>
2022-05-16 16:14:56 -0300

    Usando triggers para publicar novos dados

--- lib/food_diary_web/resolvers/meal.ex
+++ lib/food_diary_web/resolvers/meal.ex
@@ -1,14 +1,16 @@
 defmodule FoodDiaryWeb.Resolvers.Meal do
-  alias Absinthe.Subscription
+  # alias Absinthe.Subscription
   alias FoodDiary.Meals
-  alias FoodDiaryWeb.Endpoint
+  # alias FoodDiaryWeb.Endpoint
 
-  def create(%{input: params}, _context) do
-    with {:ok, meal} <- Meals.Create.call(params) do
-      Subscription.publish(Endpoint, meal, new_meal: "new_meal_topic")
-      {:ok, meal}
-    else
-      error -> error
-    end
-  end
+  # def create(%{input: params}, _context) do
+  #   with {:ok, meal} <- Meals.Create.call(params) do
+  #     Subscription.publish(Endpoint, meal, new_meal: "new_meal_topic")
+  #     {:ok, meal}
+  #   else
+  #     error -> error
+  #   end
+  # end
+
+  def create(%{input: params}, _context), do: Meals.Create.call(params)
 end
--- lib/food_diary_web/schema/types/root.ex
+++ lib/food_diary_web/schema/types/root.ex
@@ -42,10 +42,18 @@ defmodule FoodDiaryWeb.Schema.Types.Root do
   end
 
   object :root_subscription do
+    # field :new_meal, :meal do
+    #   config fn _args, _info ->
+    #     {:ok, topic: "new_meal_topic"}
+    #   end
+    # end
+
     field :new_meal, :meal do
       config fn _args, _info ->
         {:ok, topic: "new_meal_topic"}
       end
+
+      trigger :create_meal, topic: fn _ -> ["new_meal_topic"] end
     end
   end
 end
