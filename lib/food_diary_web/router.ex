7a3e8dcf641439c444f5d5b07c5c46db706cbbb3
Caayu <henrycay@outlook.com>
2022-05-15 21:12:54 -0300

    Configurando o Absinthe

--- lib/food_diary_web/router.ex
+++ lib/food_diary_web/router.ex
@@ -7,6 +7,9 @@ defmodule FoodDiaryWeb.Router do
 
   scope "/api", FoodDiaryWeb do
     pipe_through :api
+
+    forward "/graphql", Absinthe.Plug, schema: FoodDiaryWeb.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: FoodDiaryWeb.Schema
   end
 
   # Enables LiveDashboard only for development
--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule FoodDiary.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
